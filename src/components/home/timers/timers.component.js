b77e1d457220a1a00b4b310a5a586daf078543ac
Fernando Toledo <fernando_29837@live.com>
2020-04-25 12:40:46 -0500

    Adding Components

---
+++ src/components/home/timers/timers.component.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function timers() {
+    return (
+        <div>
+            <h1>Timers Section</h1>
+            <div>Search</div>
+        </div>
+    )
+}
